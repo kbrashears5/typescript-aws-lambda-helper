5fcd467af253071030c05b203d1d8533c60179b2
Kevin Brashears <kbrashears5@gmail.com>
2021-02-26 21:33:52 -0600

    migrating to eslint

--- src/test-values.ts
+++ src/test-values.ts
@@ -2,25 +2,25 @@
  * Test values
  */
 export class TestingValues {
-    // descriptions
-    public AWSError: string = 'AWS Error';
-    public InvalidTest: string = 'returns error from AWS';
-    public MustSupply: string = 'Must supply';
-    public ThrowsOnEmpty: string = 'throws on empty';
-    public ValidTest: string = 'returns valid response from AWS';
+  // descriptions
+  public AWSError: string = 'AWS Error';
+  public InvalidTest: string = 'returns error from AWS';
+  public MustSupply: string = 'Must supply';
+  public ThrowsOnEmpty: string = 'throws on empty';
+  public ValidTest: string = 'returns valid response from AWS';
 
-    // empty values
-    public EmptyString: string = '';
+  // empty values
+  public EmptyString: string = '';
 
-    // numbers
-    public NumberValue: number = 1;
+  // numbers
+  public NumberValue: number = 1;
 
-    // booleans
-    public BooleanValue: boolean = true;
+  // booleans
+  public BooleanValue: boolean = true;
 
-    // strings
-    public Arn: string = 'arn';
-    public Body: string = 'body';
-    public Name: string = 'name';
-    public Uuid: string = 'uuid';
+  // strings
+  public Arn: string = 'arn';
+  public Body: string = 'body';
+  public Name: string = 'name';
+  public Uuid: string = 'uuid';
 }
